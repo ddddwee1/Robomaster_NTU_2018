f5fca89aeded7e979f6b3d213ffa69db692526c4
GTOL <gt531593545@126.com>
2017-12-12 16:37:17 +0800

    iou fix

--- parctice/Robot_detection_v2/IOU.py
+++ parctice/Robot_detection_v2/IOU.py
@@ -5,15 +5,15 @@ def get_iou(inp1,inp2):
 	yo = min(abs(y1+h1/2-y2+h2/2), abs(y1-h1/2-y2-h2/2))
 	if abs(x1-x2) > (w1+w2)/2 or abs(y1-y2) > (h1+h2)/2:
 		return 0
-	elif abs(x1-x2) < abs(w1-w2):
+	if abs(x1-x2) < abs(w1-w2):
 		xo = min(w1, w2)
-		if abs(y1-y2) < abs(h1-h2):
-			yo = min(h1, h2)
+	if abs(y1-y2) < abs(h1-h2):
+		yo = min(h1, h2)
 	overlap = xo*yo
 	total = w1*h1+w2*h2-overlap
 	# print(overlap)
 	# print(total)
-	# print(iou)
+	# print(overlap/total)
 	return overlap/total
 
-get_iou([10,10,10,10],[10,10,1,1])
+get_iou([10,10,3,3],[10,10,1,4])
