14f41858795d0261b43390c1c3db7c16bf97b131
Ekavins <ekavins@gmail.com>
2017-12-14 19:04:31 +0800

    Drawing implementation in verification network The training session will now show the image and the region of proposals which have a confidence of more than 0

--- parctice/Robot_detection_v2/codes/train_verification.py
+++ parctice/Robot_detection_v2/codes/train_verification.py
@@ -5,16 +5,18 @@ def draw(img,c,b,wait=0):
 	# print(c.shape)
 	# print(b.shape)
 	# print(c.max())
-	for i in range(16):
-		for j in range(16):
-			if c[i][j][0]>0:
-				x = int(b[i][j][0])+j*16+8
-				y = int(b[i][j][1])+i*16+8
-				w = int(b[i][j][2])
-				h = int(b[i][j][3])
-				# print(b[i][j])
-				# cv2.circle(img,(x,y),5,(0,0,255),-1)
-				cv2.rectangle(img,(x-w,y-h),(x+w,y+h),(0,255,0),2)
+	for k in range(5):
+		color = ((255,0,0),(0,255,0),(0,0,255),(255,255,0),(0,255,255))
+		for i in range(16):
+			for j in range(16):
+				if c[k][i][j][0]>0:
+					x = int(b[k][i][j][0])+j*16+8
+					y = int(b[k][i][j][1])+i*16+8
+					w = int(b[k][i][j][2])
+					h = int(b[k][i][j][3])
+					# print(b[i][j])
+					# cv2.circle(img,(x,y),5,(0,0,255),-1)
+					cv2.rectangle(img,(x-w,y-h),(x+w,y+h),color[k],2)
 	cv2.imshow('img',img)
 	cv2.waitKey(wait)
 
@@ -36,20 +38,20 @@ def train_verification(imgholder, croppedholder, veri_conf_holder, veri_conf_los
 			
 			# Running the RPN with the original images
 			feeddict = {imgholder:img_batch}
-			c, b, f, acc = sess.run([conf, bias, feature_map, accuracy], feed_dict=feeddict)
+			c, b, f = sess.run([conf, bias, feature_map], feed_dict=feeddict)
 			
 			# Cropped the feature maps and get the confidence label
-			croppedFMs, labelConf = multipleFeatureMap(f, b, c, bias_batch, BSIZE)
+			croppedFMs, labelConf, pickedBias = multipleFeatureMap(f, b, c, bias_batch, BSIZE)
 
 			# Train the Verification Network
 			feeddict_veri = {croppedholder:croppedFMs, veri_conf_holder:labelConf}
 			sess.run([veri_conf_loss, verfication_train_step, veri_conf], feed_dict=feeddict_veri)
 			
 			if iteration%10==0
-				print('Iter:',iteration,'\tLoss:',veri_conf_loss, '\tAccuracy : ', acc)		
+				print('Iter:',iteration,'\tLoss:',veri_conf_loss)		
 
 				img = img_batch[0].astype(np.uint8)
-				draw(img,c[0],b[0],wait=5)
+				draw(img,labelConf,pickedBias,wait=5)
 
 	 
 			if iteration%5000==0 and iteration!=0:
