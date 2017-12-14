360b60423cdf1677293470b6580f3eb4d3e0ed89
ddddwee1 <cy960823@outlook.com>
2017-12-14 16:07:16 +0800

    Update graph.py

--- parctice/Robot_detection_v2/graph.py
+++ parctice/Robot_detection_v2/graph.py
@@ -9,25 +9,25 @@ def build_graph(test=False):
 	with tf.name_scope('conf_holder'):
 		conf_holder = tf.placeholder(tf.float32,[None,16,16,1]) # The confidence about 16*16 feature maps.
 	with tf.name_scope('croppedholder'):
-		croppedholder = tf.placeholder(tf.float32,[None,16,16,256]) # 256 is the number of feature maps
+		croppedholder = tf.placeholder(tf.float32,[None,4,4,256]) # 256 is the number of feature maps
 	with tf.name_scope('veri_conf_holder'):
 		veri_conf_holder = tf.placeholder(tf.float32, [None,1])
-	with tf.name_scope('veri_bias_holder'):
-		veri_bias_holder = tf.placeholder(tf.float32, [None,4]) # The veri output numbers,x,y,w,h
+	# with tf.name_scope('veri_bias_holder'):
+	# 	veri_bias_holder = tf.placeholder(tf.float32, [None,4]) # The veri output numbers,x,y,w,h
 
-	feature_map,conf, bias = RPN(imgholder,test)
-	 # = RPN_VER(feature_map)
-	
-	# cropped_map = fmCrop(feature_map)
- 	veri_conf, veri_bias = verify_net(cropped_map)
+	with tf.name_scope('mask'):
+		maskholder = tf.placeholder(tf.float32,[None,16,16,1])
+
+	conf, bias,feature_map = RPN(imgholder,test=test)
+	veri_conf, veri_bias = verify_net(croppedholder)
 	
 	bias_loss = tf.reduce_sum(tf.reduce_mean(tf.square(bias*conf_holder - bias_holder),axis=0))
 	conf_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=conf,labels=conf_holder))
 
-	veri_bias_loss = tf.reduce_sum(tf.reduce_mean(tf.square(veri_bias*veri_conf_holder - veri_bias_holder),axis=0))
+	# veri_bias_loss = tf.reduce_sum(tf.reduce_mean(tf.square(veri_bias*veri_conf_holder - veri_bias_holder),axis=0))
 	veri_conf_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=veri_conf,labels=veri_conf_holder))
 	
-	train_step = tf.train.AdamOptimizer(0.00001).minimize(bias_loss+conf_loss)
-	veri_train_step = tf.train.AdamOptimizer(0.00001).minimize(veri_bias_loss+veri_conf_loss)
+	train_step = tf.train.AdamOptimizer(0.0001).minimize(bias_loss+conf_loss)
+	veri_train_step = tf.train.AdamOptimizer(0.0001).minimize(veri_conf_loss)
 
-	return [imgholder,bias_holder, conf_holder], [croppedholder, veri_bias_holder, veri_conf_holder], [bias_loss, conf_loss], [veri_bias_loss, veri_conf_loss], [train_step, veri_train_step], [conf, bias] , [veri_conf,veri_bias]
+	return [imgholder,bias_holder, conf_holder,maskholder], [croppedholder, veri_conf_holder], [bias_loss, conf_loss], [veri_conf_loss], [train_step, veri_train_step], [conf, bias] , [veri_conf], feature_map
