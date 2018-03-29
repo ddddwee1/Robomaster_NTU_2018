0645ff27e0c1b830dc82dd47e56dd1604b8657b6
ddddwee1 <cy960823@outlook.com>
2018-03-29 10:54:17 +0800

    update lidar

--- parctice/lidar/ctr.py
+++ parctice/lidar/ctr.py
@@ -1,7 +1,7 @@
 import serial 
 import RPLIDAR_CONST
 
-ser = serial.Serial('COM7',115200)
+ser = serial.Serial('COM3',115200)
 def read_msg():
 	while 100:
 		data = ser.read(5)
@@ -14,7 +14,20 @@ def start():
 	data = ser.read(7)
 	print(data)
 	print('-----response-----')
+	return data
+
+def compile_msg(data):
+	angle = data[2] + data[1]
+	angle = angle >> 1
+	distance = data[4] + data[3]
+
+	angle = np.frombuffer(angle,dtype=np.uint16)
+	distance = np.frombuffer(distance,dtype=np.uint16)
+
+	print('ang:',angle/64.)
+	print('dist:',distance/4.)
 
 # print()
 start()
-read_msg()
+dat = read_msg()
+compile_msg(dat)
