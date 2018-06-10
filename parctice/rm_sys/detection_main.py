29d48527388fe34d8ee555d3cc0fe334d16da58b
Cheng Yu <cy960823@outlook.com>
2018-06-10 15:37:27 +0800

    update detection code

---
+++ parctice/rm_sys/detection_main.py
@@ -0,0 +1,23 @@
+from PID.PID2d import PID2d
+import camera_module
+import detection_mod
+import util
+import threading
+import robot_prop 
+
+class detection_non_thread():
+	def __init__(self):
+		self.pid = PID2d(2.28,0.000,-0.0000228)
+		self.camera_thread = camera_module.camera_thread()
+		self.camera_thread.start()
+
+	def get_detection(self):
+		img = self.camera_thread.read()
+		coord = detection_mod.get_coord_from_detection(img)
+		v1,v2,shoot = util.get_v_and_shoot(coord,self.pid)
+		v = [v1,v2]
+		shoot = shoot
+		robot_prop.v1 = v1 
+		robot_prop.v2 = v2 
+		robot_prop.shoot = shoot
+		return v,shoot
