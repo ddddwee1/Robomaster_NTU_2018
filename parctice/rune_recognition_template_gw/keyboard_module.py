97861cbfcad2221152e454c281c515106d741bf0
kyubey <wonggw123@gmail.com>
2018-06-29 05:46:08 +0800

    corrected indentation errors

--- parctice/rune_recognition_template_gw/keyboard_module.py
+++ parctice/rune_recognition_template_gw/keyboard_module.py
@@ -4,27 +4,28 @@ import time
 
 
 class keyboard_thread(threading.Thread):
-    def __init__(self):
-        self.settings = termios.tcgetattr(sys.stdin)
-        self.key = ''
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
-        threading.Thread.__init__(self)
+	def __init__(self):
+
+		self.settings = termios.tcgetattr(sys.stdin)
+		self.key = ''
+		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
+		threading.Thread.__init__(self)
 
 	def run(self):
 		while True:
-			time.sleep(0.03)
+			time.sleep(0.06)
 			self.key = self.getKey()
 
-    def getKey(self):
-        tty.setraw(sys.stdin.fileno())
-        rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
-        if rlist:
-            key = sys.stdin.read(1)
-        else:
-            key = ''
+	def getKey(self):
+		tty.setraw(sys.stdin.fileno())
+		rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+		if rlist:
+			key = sys.stdin.read(1)
+		else:
+			key = ''
 
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
-        return key
+		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
+		return key
 
-    def read(self):
-        return self.key
+	def read(self):
+		return self.key
