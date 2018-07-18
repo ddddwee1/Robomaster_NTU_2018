e725a36fe6c2627b81eec17deb3e8864c19ceb3c
Kyubey <wonggw123@gmail.com>
2018-07-18 10:34:31 +0800

    updated buff detection

--- parctice/New_protocol/rm_sys_infantry_new_control/rune_module/digit_detection.py
+++ parctice/New_protocol/rm_sys_infantry_new_control/rune_module/digit_detection.py
@@ -15,6 +15,12 @@ image_height = 480
 kernel2 = np.ones((2,2),np.uint8)
 kernel3 = np.ones((3,3),np.uint8)
 
+def hist_equal(img):
+	equ = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	equ[:,:,2] = cv2.equalizeHist(equ[:,:,2])
+	equ = cv2.cvtColor(equ, cv2.COLOR_HSV2BGR)
+	return equ
+
 def get_digits(image,bigbuff=False):
 
 	leftRect = []
@@ -33,15 +39,15 @@ def get_digits(image,bigbuff=False):
 	#_,th3 = cv2.threshold(blurred,200,255,cv2.THRESH_BINARY)# +cv2.THRESH_OTSU)
 	edged = cv2.Canny(blurred, 200, 240)
 	
-	#cv2.imshow('',image)
+	cv2.imshow('',image)
 
-	#cv2.waitKey(1)
+	cv2.waitKey(1)
 	_, contours, hierarchy = cv2.findContours(edged.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
 	for contour in contours:
 		peri = cv2.arcLength(contour,True)
 		contour = cv2.approxPolyDP(contour, 0.0001 * peri, True)
-		if cv2.contourArea(contour)<60 or cv2.contourArea(contour)>600:
+		if cv2.contourArea(contour)<100 or cv2.contourArea(contour)>1000:
 			continue
 
 		x,y,w,h = cv2.boundingRect(contour)
@@ -137,14 +143,14 @@ def get_digits(image,bigbuff=False):
 	M = cv2.getPerspectiveTransform(pts1,pts2)
 
 	dst = cv2.warpPerspective(image,M,(300,200))
-	cv2.imshow('aaa',dst)
-	cv2.waitKey(1)
+	#cv2.imshow('aaa',dst)
+	#cv2.waitKey(1)
 
 	""" 
 	7segment
 	"""
 
-	digits_7seg_rect = [(100, 0), (121, 0), (142, 0), (162, 0), (183, 0)]
+	digits_7seg_rect = [(98, 0), (119, 0), (140, 0), (161, 0), (182, 0)]
 
 	digit_7seg_imgs = []
 
@@ -152,22 +158,17 @@ def get_digits(image,bigbuff=False):
 		#cv2.rectangle(dst,(x,y),(x+19,y+34),(255,0,0),1)
 		img_sevseg = dst[y:y+34,x:x+19]
 
-		img_sevseg=cv2.cvtColor(img_sevseg, cv2.COLOR_BGR2HLS)
-		#cv2.imshow('bbccc',img_sevseg)
-		img_sevseg_red = img_sevseg[:,:,1].copy()
-		#cv2.imshow('bbccc',img_sevseg_red)
-		img_sevseg_red = cv2.morphologyEx(img_sevseg_red, cv2.MORPH_OPEN, kernel2)
-		_,buf = cv2.threshold(img_sevseg_red,150,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-		buf = cv2.copyMakeBorder(buf, 0, 10 , 0, 0, cv2.BORDER_CONSTANT, value=BLACK)
-		buf = cv2.bitwise_not(buf)
-		buf=cv2.erode(buf,kernel3,iterations = 1)
-		buf=cv2.dilate(buf,kernel2,iterations = 2)
-		#buf=cv2.erode(buf,kernel2,iterations = 1)
+		# another test
+		img_sevseg = img_sevseg[:,:,2]
+		img_sevseg = cv2.inRange(img_sevseg,215,255)
+		img_sevseg = cv2.morphologyEx(img_sevseg,cv2.MORPH_CLOSE,kernel2)
+
+		buf = cv2.bitwise_not(img_sevseg)
 		buf = cv2.resize(buf,(24,24))
 		buf = cv2.copyMakeBorder(buf, 1, 3, 1, 3, cv2.BORDER_CONSTANT, value=WHITE)
 
-		#cv2.imshow('bb',buf)
-		#cv2.waitKey(0)
+		cv2.imshow('bb',buf)
+		cv2.waitKey(0)
 
 		buf = buf.reshape([-1])
 		buf = 255 - buf
@@ -176,7 +177,7 @@ def get_digits(image,bigbuff=False):
 		digit_7seg_imgs.append(buf)
 
 	#cv2.imshow('aaa',dst)
-	#cv2.waitKey(1)
+	#cv2.waitKey(0)
 
 	scr_7seg_raw = conv_deploy.get_pred_7seg(digit_7seg_imgs)
 	#print (scr_7seg_raw)
@@ -185,20 +186,20 @@ def get_digits(image,bigbuff=False):
 		""" 
 		Handwritten
 		"""
-		digits_rect = [(51,55),(125,55),(200,55),(51,110),(125,110),(200,110),(51,164),(125,164),(200,164)]
+		digits_rect = [(50,53),(124,53),(200,53),(50,108),(124,108),(200,108),(50,162),(124,162),(200,162)]
 		digit_imgs = []
 		abc = 0
 		for x,y in digits_rect:
-			#cv2.rectangle(dst,(x,y),(x+51,y+33),(0,0,255),1)
-			buf =  dst[y:y+33,x:x+51]
+			#cv2.rectangle(dst,(x,y),(x+53,y+36),(0,0,255),1)
+			buf =  dst[y:y+37,x:x+52]
 			buf = cv2.cvtColor(buf,cv2.COLOR_BGR2GRAY)
 			_,buf = cv2.threshold(buf,20,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-			buf = cv2.erode(buf,kernel2,iterations = 1)
+			buf = cv2.erode(buf,kernel2,iterations = 2)
 			buf = cv2.resize(buf,(24,24))
-			buf = cv2.copyMakeBorder(buf, 2, 2, 2, 2, cv2.BORDER_CONSTANT, value=WHITE)
+			buf = cv2.copyMakeBorder(buf, 1, 3, 2, 2, cv2.BORDER_CONSTANT, value=WHITE)
 
-			#cv2.imshow('cv',buf)
-			#cv2.waitKey(0)
+			# cv2.imshow('cv',buf)
+			# cv2.waitKey(0)
 
 			buf = 255 - buf
 			buf = np.float32(buf) / 255.
@@ -209,7 +210,7 @@ def get_digits(image,bigbuff=False):
 		#cv2.waitKey(1)
 
 		handwritten_num_raw = conv_deploy.get_pred(digit_imgs)
-		#print(handwritten_num_raw)
+		# print(handwritten_num_raw)
 
 		handwritten_dict = {}
 
@@ -232,17 +233,17 @@ def get_digits(image,bigbuff=False):
 		Flaming Digits
 		"""
 
-		flamingdigits_rect = [(61,55),(135,55),(210,55),(61,110),(135,110),(210,110),(61,164),(135,164),(210,164)]
+		flamingdigits_rect = [(59,54),(134,54),(208,54),(59,109),(134,109),(208,109),(59,162),(134,162),(208,162)]
 		digit_imgs = []
 		abc = 0
 		for x,y in flamingdigits_rect:
-			#cv2.rectangle(dst,(x,y),(x+30,y+33),(255,555,255),1)
-			buf =  dst[y:y+33,x:x+30]
+			#cv2.rectangle(dst,(x,y),(x+32,y+35),(255,555,255),1)
+			buf =  dst[y:y+35,x:x+32]
 			img_FD = cv2.cvtColor(buf,cv2.COLOR_BGR2GRAY)
 			_,buf = cv2.threshold(img_FD,200,255,cv2.THRESH_TOZERO+cv2.THRESH_OTSU)
 			edged = cv2.Canny(buf, 255, 255)
 			_, contours, hierarchy = cv2.findContours(edged.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-			cv2.drawContours(buf, contours, -1, 255,-1)
+			cv2.drawContours(buf, contours, -1, 255 , -1)
 			buf = cv2.bitwise_not(buf)
 			buf = cv2.resize(buf,(24,24))
 			buf = cv2.copyMakeBorder(buf, 2, 2, 2, 2, cv2.BORDER_CONSTANT, value=WHITE)
@@ -275,5 +276,17 @@ def get_digits(image,bigbuff=False):
 
 		handwritten_num = [-1]
 		#print(scr_FD)
-
+	cv2.imshow('aaa',dst)
+	cv2.waitKey(0)
 	return coord, scr_7seg_raw, handwritten_num, Flaming_digit
+
+if __name__=='__main__':
+	for i in range(1000):
+		img = cv2.imread('imgs/%d.jpg'%i)
+		coord, scr_7seg_raw, handwritten_num, Flaming_digit = get_digits(img)
+		print(i)
+		print(coord)
+		print(scr_7seg_raw)
+		print(handwritten_num)
+		print(Flaming_digit)
+		print('-----------------')
